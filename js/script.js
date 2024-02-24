5718441994f717c7646aee3bbc9ad4721f905eca
Misha <movkata6@mail.ru>
2024-02-24 22:46:10 +0300

    progect3

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 'use strict'
-
 //1
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); // + будет преобразовать в число
-console.log(numberOfFilms);
+'use strict'
 
-//2
+
+
+
+//
+let numberOfFilms; // + будет преобразовать в число
+
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); // + будет преобразовать в число
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) { //isNaN() - число или нет
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: { //пустой объект
@@ -18,14 +30,90 @@ const personalMovieDB = {
     ],
     privat: false
 };
+// ЗАДАНИЕ №2
+function ShowMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+ShowMyDB(personalMovieDB.privat);
 
+// function ShowMyDB() {
+//     if (personalMovieDB.privat == false) {
+//         console.log(personalMovieDB);
+//     }
+// }
+// ShowMyDB();
+
+// ЗАДАНИЕ №3
+function writeYourGenres() {
+
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номеров ${i}?`); // i - 1  убираем пустой элемент массива
+    }
+
+}
+writeYourGenres();
+
+
+//1 and 2 and 4
+// let i = 0;
+// do {
+
+//     const lostMovie = prompt('Один из последних фильмов?', ''), //если нажать на отменить ответ, то вывется null
+//         movieRating = prompt('На сколько оцените его?', '');
+//     if (lostMovie != null && movieRating != null && lostMovie != '' && movieRating != '' && lostMovie.length < 50) {
+//         personalMovieDB.movies[lostMovie] = movieRating;
+//         console.log("done");
+//     } else {
+//         console.log("error");
+//         i--; //возвращаесмя на один шаг назад, чтобы повторить вопросы
+//     }
+//     i++
+// } while (i < 2)
+// let i = 0;
+// while (i < 2) {
+
+//     const lostMovie = prompt('Один из последних фильмов?', ''), //если нажать на отменить ответ, то вывется null
+//         movieRating = prompt('На сколько оцените его?', '');
+//     if (lostMovie != null && movieRating != null && lostMovie != '' && movieRating != '' && lostMovie.length < 50) {
+//         personalMovieDB.movies[lostMovie] = movieRating;
+//         console.log("done");
+//     } else {
+//         console.log("error");
+//         i--; //возвращаесмя на один шаг назад, чтобы повторить вопросы
+//     }
+//     i++
+// }
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const lostMovie = prompt('Один из последних фильмов?', ''), //если нажать на отменить ответ, то вывется null
+            movieRating = prompt('На сколько оцените его?', '');
+        if (lostMovie != null && movieRating != null && lostMovie != '' && movieRating != '' && lostMovie.length < 50) {
+            personalMovieDB.movies[lostMovie] = movieRating;
+            console.log("done");
+        } else {
+            console.log("error");
+            i--; //возвращаесмя на один шаг назад, чтобы повторить вопросы
+        }
+
+    }
+}
+rememberMyFilms();
 //3
-const lostMovie = prompt('Один из последних фильмов?', ''),
-    movieRating = +prompt('На сколько оцените его?', ''),
-    lostMovie1 = prompt('Один из последних фильмов?', ''),
-    movieRating1 = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[lostMovie] = movieRating; //logan:8.1
-personalMovieDB.movies[lostMovie1] = movieRating1;
 
-console.log(personalMovieDB);
+function detectLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('мало');
+
+    } else if (personalMovieDB.count < 30) {
+        console.log('Вы классик');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('error');
+    }
+}
+detectLevel();
